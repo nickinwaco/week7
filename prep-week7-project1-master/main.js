370c80bb9f77b8d90c4893cd84e6206ba28a1eec
nick rodriguez <nickinwaco@gmail.com>
2017-01-10 11:20:52 -0600

    update files
    update files

--- prep-week7-project1-master/main.js
+++ prep-week7-project1-master/main.js
@@ -12,23 +12,54 @@ with the data and update the page accordingly.
 Have fun!
 =======================================================
 */
-$(document).ready(function() {
+// $(document).ready(function() {
+//
+//
+// //1. OMDB call
+// $('#submitBtn').click(function() {
+//     var xhr = new XMLHttpRequest();
+//     xhr.addEventListener('load', function() {
+//         if (xhr.status !== 200) {
+//             console.log("success")
+//             return;
+//         }
+//         var data = JSON.parse(xhr.responseText);
+//     // console.log(data["imdbID"]);
+//     });
+//     xhr.open('GET', 'http://www.omdbapi.com/?t=Frozen');
+//     xhr.send();
+//     //console.log(data);
+//       console.log(xhr);
+//
+//     });
+//
+//
+//
+//
+//
+//
+//
+// });  // end of JQUERY
 
-//1. OMDB call
-$('#submitBtn').click(function() {
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', function() {
-        if (xhr.status !== 200) {
-            console.log("success")
-            return;
-        }
-        var data = JSON.parse(xhr.responseText);
-     console.log(data["title"]);
-    });
-    xhr.open('GET', 'http://www.omdbapi.com/?t=Frozen');
-    xhr.send();
-  //  console.log(data.title);
-  //     console.log(xhr);
-  //  console.log("test");
-    });
-});
+
+
+
+// Google Maps
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {
+        lat: 40.771664,
+        lng: -73.966275
+
+//40.786288,-73.971382
+//40.771664,-73.966275
+      },
+    zoom: 18,
+  //mapTypeId: google.maps.MapTypeId.SATELLITE,
+    // 90' North is Top of Page
+    heading: 90,
+    // Tilt map 45 degrees vs 0
+    tilt: 45,
+  });
+
+}
